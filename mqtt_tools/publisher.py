e7821f2fe969cb58eb307b8941a09191d4a35bae
akipur <rupikasinha23@gmail.com>
2025-02-13 19:51:08 +0530

    add-on

--- mqtt_tools/publisher.py
+++ mqtt_tools/publisher.py
@@ -3,25 +3,25 @@ import sys
 import paho.mqtt.client as mqtt
 
 class Publisher:
-	def __init__(hostname = "localhost", port = 1833, keep_alive = 60):
+	def __init__(self, hostname = "localhost", port = 1833, keep_alive = 60):
 		self.client = mqtt.Client()
 		self.hostname = hostname
 		self.port = port
 		self.keep_alive = keep_alive
 
-	def publish_message(topic = "", message = ""):
+	def publish_message(self, topic = "", message = ""):
 		try:
 			self.client.publish(topic, message, 0)
 		except:
 			print("Error sending message to topic")
 
-	def connect():
+	def connect(self):
 		try:
 			self.client.connect(self.hostname, self.port, self.keep_alive)
 		except:
 			print("Error connecting")
 
-	def disconnect():
+	def disconnect(self):
 		try:
 			self.client.disconnect()
 		except:
--- mqtt_tools/subscriber.py
+++ mqtt_tools/subscriber.py
@@ -3,24 +3,24 @@ import sys
 import paho.mqtt.client as mqtt
 
 class Subscriber:
-	def __init__(hostname = "localhost", port = 1883, keep_alive = 60, message_handling = None):
+	def __init__(self, hostname = "localhost", port = 1883, keep_alive = 60, message_handling = None):
 		self.client = mqtt.Client()
 		self.hostname = hostname
 		self.port = port
 		self.keep_alive = keep_alive
 		self.client.on_message = message_handling
 
-	def subscribe_to(topic = ""):
+	def subscribe_to(self, topic = ""):
 		self.client.subscribe(topic)
 
-	def connect():
+	def connect(self):
 		try:
 			self.client.connect(self.hostname, self.port, self.keep_alive)
 			self.client.loop_forever()
 		except:
 			print("Unable to connect to host")
 
-	def disconnect():
+	def disconnect(self):
 		try:
 			self.client.disconnect()
 		except:
