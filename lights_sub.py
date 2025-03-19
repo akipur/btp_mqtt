b57606de02d4c75471e3b8526996fdebcc2506ad
akipur <rupikasinha23@gmail.com>
2025-03-19 17:51:30 +0530

    .

--- lights_sub.py
+++ lights_sub.py
@@ -3,8 +3,8 @@ from gpiozero import LED
 import time
 
 class LED_control(Subscriber):
-	def __init__(self, hostname = "mqtt.eclipseprojects.io", port = 1833, keep_alive = 60):
-		Subscriber.__init__(hostname, port, keep_alive)
+#	def __init__(self, hostname = "mqtt.eclipseprojects.io", port = 1833, keep_alive = 60):
+#		Subscriber.__init__(hostname, port, keep_alive)
 
 	def take_action(msg):
     		print(f"Received message on topic {message.topic}: {message.payload}")
@@ -15,7 +15,7 @@ class LED_control(Subscriber):
 
 led = LED(18)
 led.on()
-subscriber = LED_Control()
+subscriber = LED_control()
 subscriber.connect()
 subscriber.subscribe("light_control")
 
